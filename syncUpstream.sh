b8fbc3fb64faa18ed46918c7a2e76036c214d654
Sammy Patenotte <sammy.patenotte@yahoo.com>
2015-08-10 16:55:49 -0700

    Created syncUpstream.sh script to automatically sync upstream changes

---
+++ syncUpstream.sh
@@ -0,0 +1,15 @@
+git fetch upstream
+git merge upstream/master
+git push
+cd .repo/manifests
+git fetch upstream
+git merge upstream/master
+git push
+cd ../../gaia
+git fetch upstream
+git merge upstream/master
+git push
+cd ../gecko
+git fetch upstream
+git merge upstream/master
+git push
